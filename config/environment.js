8081da26e6d6e656936f60668a32d03be55b31d3
Roni Dover <roni.dover@gmail.com>
2016-10-19 15:59:12 +0300

    fixing typo

--- config/environment.js
+++ config/environment.js
@@ -6,7 +6,7 @@ module.exports = function(environment) {
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
-    namespace: 'api'
+    namespace: 'api',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
